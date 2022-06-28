658daf687e631305a0d9e336fcf3bdb984835818
chrisdhodges12 <102702357+chrisdhodges12@users.noreply.github.com>
2022-06-28 15:51:34 -0400

    add queries to create, read, update, and delete candidates

--- server.js
+++ server.js
@@ -19,11 +19,40 @@ const db = mysql.createConnection(
       database: 'election'
     },
     console.log('Connected to the election database.')
-  );
+);
 
-  db.query(`SELECT * FROM candidates`, (err, rows) => {
-      console.log(rows);
-  });
+//Get all candidates
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+
+//Get single candidate
+// db.query(`SELECT * FROM candidates Where id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+//Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+//Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//              VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
 
 //Default response for any other request (Not Found)
 app.use((req, res) => {
